6e70c60ab98b8505f5bbbf32ae5506df22c6a7f7
Aditya Alok <dev.aditya.alok@gmail.com>
2021-07-25 15:52:02 +0530

    Fix: imports by isort and reduce interrogate fail to 80%

--- src/sylvie/interpreter/syntax_directed/parser.py
+++ src/sylvie/interpreter/syntax_directed/parser.py
@@ -1,33 +1,58 @@
+"""This module parses the lexed tokens and evaluates the expression.
+
+    Example:
+        calc >> --7 + 3 * (10 / (12 / (3 + 1) - 1))
+        22
+
+    Grammar:
+        sub_expr  : mul ((PLUS | MINUS) mul)*
+        mul       : term ((MUL | DIV) term))*
+        term      : (PLUS | MINUS)* factor
+        factor    : NUMBER | LPAREN sub_expr RPAREN
+"""
+
 from typing import Union
+
 from sylvie.interpreter.syntax_directed.lexer import Lexer
 from sylvie.interpreter.tokens import TokenType
 
 
 class Parser:
     def __init__(self, text: str) -> None:
-        """Parse tokens and evaluate expression."""
+        """Parses tokens and evaluates the expression formed."""
         self.tokens = Lexer(text).get_tokens()
+        self.column = 0
         self.advance()
 
     def advance(self) -> None:
+        """Move to next token and increase column count."""
         try:
             self.current_token = next(self.tokens)
-            # print("advance called :", self.current_token)
+            self.column += 1
         except StopIteration:
             pass
-            # self.current_token = Token(TokenType.EOF)
 
     @staticmethod
-    def error() -> None:
-        raise SyntaxError("SyntaxError: invalid syntax")
+    def error(msg: str) -> None:
+        raise SyntaxError(msg)
 
     def eat(self, expected_token: TokenType) -> None:
+        """Validated current token and advance to next token.
+
+        Checks if current token is the the token we expect to be present,
+        if not then raise SyntaxError.
+        """
         if self.current_token.type == expected_token:
             self.advance()
         else:
-            self.error()
+            self.error(
+                "Unexpected character '{}' at column {}, expected '{}'".format(
+                    self.current_token.value, self.column, expected_token.value
+                )
+            )
 
     def factor(self) -> Union[int, float]:
+        """factor : NUMBER | LPAREN sub_expr RPAREN"""
         token = self.current_token
         if token.type == TokenType.NUMBER:
             self.eat(TokenType.NUMBER)
@@ -35,11 +60,12 @@ class Parser:
 
         # if is not NUMBER then has to be parenthesis
         self.eat(TokenType.LPAREN)
-        result = self.expr()
+        result = self.sub_expr()
         self.eat(TokenType.RPAREN)
         return result
 
     def term(self) -> Union[int, float]:
+        """term : (MINUS | PLUS)* factor"""
         minus_count = 0
         while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
             if self.current_token.type == TokenType.PLUS:
@@ -52,6 +78,7 @@ class Parser:
         return (0 - result) if minus_count % 2 else result
 
     def mul(self):
+        """mul : term ((MUL | DIV) term)*"""
         result = self.term()
         while self.current_token.type in (TokenType.MULTIPLY, TokenType.DIV):
             if self.current_token.type == TokenType.MULTIPLY:
@@ -66,7 +93,8 @@ class Parser:
                     raise
         return result
 
-    def expr(self) -> Union[int, float]:
+    def sub_expr(self) -> Union[int, float]:
+        """sub_expr: mul ((PLUS | MINUS) mul)*"""
         result = self.mul()
 
         while self.current_token.type in (
@@ -80,6 +108,15 @@ class Parser:
             elif token.type == TokenType.MINUS:
                 self.eat(TokenType.MINUS)
                 result -= self.mul()
+        return result
+
+    def expr(self):
+        """Parsed arithmetic expression."""
+        result = self.sub_expr()
         if self.current_token.type != TokenType.EOF:
-            self.error()
+            self.error(
+                "Unexpected character '{}' at column {}".format(
+                    self.current_token.value, self.column
+                )
+            )
         return result
--- src/sylvie/interpreter/tokens.py
+++ src/sylvie/interpreter/tokens.py
@@ -1,25 +1,31 @@
+"""This modules contains classes for token handling.
+
+class TokenType(Enum): is an enum representing valid tokens.
+
+class Token : a dataclass representing a token with it's value and type.
+"""
+
 from dataclasses import dataclass
-from enum import IntEnum, unique
+from enum import Enum
 from typing import Union
 
 
-@unique
-class TokenType(IntEnum):
-    PLUS = 1
-    MINUS = 2
-    DIV = 3
-    MULTIPLY = 4
-    NUMBER = 5
-    RPAREN = 6
-    LPAREN = 7
-    EOF = 8
+class TokenType(Enum):
+    """Enum representing valid tokens."""
+
+    PLUS = "+"
+    MINUS = "-"
+    DIV = "/"
+    MULTIPLY = "*"
+    NUMBER = "REAL_CONST"
+    RPAREN = ")"
+    LPAREN = "("
+    EOF = "EOF"
 
 
 @dataclass
 class Token:
-    type: TokenType
-    value: Union[int, float, None] = None
+    """Dataclass representing a token with it's type and value."""
 
-    def __repr__(self) -> str:
-        """String representation of token object."""
-        return f"{self.type.name}: {self.value if self.value else ''}"
+    type: TokenType
+    value: Union[int, float, str]
